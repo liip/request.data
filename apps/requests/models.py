e608f946db8a9f317a68036a18b1c0565c5dc4f5
Rachel Knowler <rachel.knowler@liip.ch>
2014-02-27 20:04:40 +0100

    Added unicode methods to models.

--- apps/requests/models.py
+++ apps/requests/models.py
@@ -7,21 +7,30 @@ from django.conf import settings
 class User(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField(max_length=255)
+    def __unicode__(self):
+        return self.name
     
 class Agency(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField(max_length=255)
+    def __unicode__(self):
+        return self.name
     
 class Request(models.Model):
+    title = models.CharField(max_length=255)
     description = models.TextField(blank=False)
     created = models.DateTimeField(auto_now_add=True)
     agency = models.ForeignKey(Agency)  
     users = models.ManyToManyField(User, related_name="requests")
     creator = models.ForeignKey(User, related_name="created_requests")
+    def __unicode__(self):
+        return self.title
     
 class Comment(models.Model):
     description = models.TextField(blank=False)
     created = models.DateTimeField(auto_now_add=True)
     creator = models.ForeignKey(User, related_name="comments")
     request = models.ForeignKey(Request, related_name="comments")
+    def __unicode__(self):
+        return self.description[:80]
 
