6e3462ef09559f0712ab4b64af805cd3eed1b4ca
Philipp Küng <git@philippkueng.ch>
2014-03-03 11:15:46 +0100

    adding casperjs tests and setup instructions into the chef recipe

--- cookbooks/vagrant/recipes/default.rb
+++ cookbooks/vagrant/recipes/default.rb
@@ -76,7 +76,6 @@ bash "install CasperJs " do
     set -e
     git clone git://github.com/n1k0/casperjs.git
     cd casperjs
-    git checkout tags/1.0.2
     ln -sf `pwd`/bin/casperjs /usr/local/bin/casperjs
   EOH
 end
---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+casperjs test tests/
+
+# TESTCMD="casperjs test --server=http://$HOSTNAME --includes=$INCLUDES_STRING --verbose"
