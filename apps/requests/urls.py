bf66e8df41eebdd577ad866cf36173fe7b636da1
Rachel Knowler <rachel.knowler@liip.ch>
2014-03-07 11:47:43 +0100

    Renamed home to index.

--- apps/requests/urls.py
+++ apps/requests/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^requests/([0-9]+)/?$', 'apps.requests.views.request_detail'),
     url(r'^requests/([A-Za-z]+)/?$', 'apps.requests.views.request_list'),
-    url(r'^$', 'apps.requests.views.home'),
+    url(r'^$', 'apps.requests.views.index'),
 )
