5e8eaa8fde0f17697d23319a76e6593dbee15508
Rachel Knowler <rachel.knowler@liip.ch>
2014-03-07 11:11:20 +0100

    Comment form.

--- apps/requests/forms.py
+++ apps/requests/forms.py
@@ -19,8 +19,15 @@ class UserForm(ModelForm):
         fields = ['name', 'email']
 
 class AgencyForm(ModelForm):
-    a_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Which agency are you asking? (If not found in dropdown.)'}), blank=True)
+    a_name = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder': 'Which agency are you asking? (If not found in dropdown.)'}))
 
     class Meta:
         model = Agency
         fields = ['a_name']
+
+class CommentForm(ModelForm):
+    description = forms.CharField(max_length=5000, widget=forms.Textarea())
+    
+    class Meta:
+        model = Comment
+        fields = ['description']
